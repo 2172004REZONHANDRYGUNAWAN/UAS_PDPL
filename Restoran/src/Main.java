d4e598649ad112422e97228361354e2a2c07bf1f
2172004REZONHANDRYGUNAWAN <90677382+2172004REZONHANDRYGUNAWAN@users.noreply.github.com>
2023-08-30 21:19:07 +0700

    zsdfghjkl
    wsdfghjkl;

--- Restoran/src/Main.java
+++ Restoran/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Selamat datang di restoran Maranatha");
+        System.out.println("Kami tahu anda pasti lapar");
+        System.out.println("Silahkan Pilih :");
+        System.out.println("1. Makanan");
+        System.out.println("2. Minuman");
+        System.out.println("3. Dessert");
+        System.out.println("4. Exit");
+
     }
 }
